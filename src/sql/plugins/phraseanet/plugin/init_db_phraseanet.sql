4c6cd8bf25cbf088d4f44e63d06fc413adb323bc
(no author) <(no author)@bab10101-e421-0410-a517-8ce0973de3ef>
2013-02-25 10:36:05 +0000

    PHRASEANET-10 : update audio and picture template
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-phraseanet@47097 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/phraseanet/plugin/init_db_phraseanet.sql
+++ src/sql/plugins/phraseanet/plugin/init_db_phraseanet.sql
@@ -15,9 +15,9 @@ VALUES (1, 'Template pour les videos', '
 </p>
 ', 'video' );
 INSERT INTO phraseanet_template ( id_template, name, default_template, media_type)
-VALUES (2, 'Template pour les audios', '', 'audio' );
+VALUES (2, 'Template pour les audios', '<object type="application/x-shockwave-flash" data="plugins/phraseanet/swf/dewplayer/dewplayer-mini.swf?mp3=${embed.getEmbedItem(\'preview\').permalink.url}" width="160" height="20" id="dewplayer-mini"><param name="wmode" value="transparent" /><param name="movie" value="plugins/phraseanet/swf/dewplayer/dewplayer-mini.swf?mp3=${embed.getEmbedItem(\'preview\').permalink.url}" /></object>', 'audio' );
 INSERT INTO phraseanet_template ( id_template, name, default_template, media_type)
-VALUES (3, 'Template pour les images', '<img src="${embed.getEmbedItem(\'preview\').permalink.url}" width="${embed.getEmbedItem(\'preview\').width}" height="${embed.getEmbedItem(\'preview\').height}" />', 'image' );
+VALUES (3, 'Template pour les images', '<a href="${embed.getEmbedItem(\'preview\').permalink.url}" target="_blank" title="Agrandir"><img src="${embed.getEmbedItem(\'thumbnail\').permalink.url}" alt="Image : ${embed.getEmbedItem(\'preview\').permalink.label}" width="${embed.getEmbedItem(\'thumbnail\').width}px" height="${embed.getEmbedItem(\'thumbnail\').height}px" /></a>', 'image' );
 INSERT INTO phraseanet_template ( id_template, name, default_template, media_type)
 VALUES (4, 'Template pour les flashs', '', 'flash' );
 INSERT INTO phraseanet_template ( id_template, name, default_template, media_type)
