5a494c2aaae4c9f62212149a5e54e581ab9a63a6
(no author) <(no author)@bab10101-e421-0410-a517-8ce0973de3ef>
2013-02-25 09:04:54 +0000

    PHRASEANET-12 : Update flowplayer with playlist.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-phraseanet@47096 bab10101-e421-0410-a517-8ce0973de3ef

--- webapp/js/plugins/phraseanet/player.js
+++ webapp/js/plugins/phraseanet/player.js
@@ -6,3 +6,12 @@ flowplayer("a.phraseaPlayer", "js/plugins/phraseanet/flowplayer/flowplayer-3.2.1
         }
     }
 });
+
+$(function() {
+$f("playerMulti", "js/plugins/phraseanet/flowplayer/flowplayer-3.2.15.swf", {
+   clip: {
+      // baseUrl: 'http://stream.flowplayer.org'
+    }
+    // playlist plugin
+    }).playlist("#playlist");
+});
